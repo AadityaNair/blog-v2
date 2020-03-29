6e8add7e734f67bcdef1b1906fec9c2067adf3a3
AadityaNair <aadityanair6494@gmail.com>
2020-03-30 01:57:36 +0530

    Added stubs

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,37 +4,20 @@ import React from "react"
 export default () => (
   // <Head/>
   <div>
-    <section class="hero">
-      <div class="hero-inner">
-        <div class="container">
-          <img
-            src="Test image"
-            alt="author thumb"
-            class="dot"
-          />
-          <h1>
-            end2end <small>v2.0</small>
-          </h1>
-          <h3>
-            I'm <strong itemprop="name">Test Name</strong>, a
-            <em itemprop="jobTitle">Test Job</em>.
-          </h3>
-          <h3>Test Bio</h3>
-          <a href="/about" class="button button-ghost" title="about">
-            about
-          </a>
-          <a href="/blog" class="button button-ghost" title="blog">
-            blog
-          </a>
-          <a
-            href="/archive/master.zip"
-            class="button button-ghost"
-            title="Download"
-          >
-            Download
-          </a>
-        </div>
-      </div>
-    </section>
+    <div class="hero-inner">
+      <img src="Test image" alt="author thumb" class="dot" />
+      <h1>end2end v2.0</h1>
+      <h3>
+        I'm <strong itemprop="name">Test Name</strong>, a
+        <em itemprop="jobTitle">Test Job</em>.
+      </h3>
+      <h3>Test Bio</h3>
+      <a href="/about" class="button button-ghost" title="about">
+        about
+      </a>
+      <a href="/blog" class="button button-ghost" title="blog">
+        blog
+      </a>
+    </div>
   </div>
 )
