9f0dd29adf8bc8ce3ec9075ca2b67fed4c1913bf
AadityaNair <aadityanair6494@gmail.com>
2020-04-05 02:03:00 +0530

    Basic Header stuff

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,2 +1,15 @@
 import React from "react"
+import { Link } from "gatsby"
 
+export default ({ props }) => (
+  <header>
+    {/* use the below link to go back  */}
+    <Link to="/">Go back</Link>
+    <h1>
+      <Link to="/">The Blog of Nair</Link>
+    </h1>
+
+    <Link to="/about">about</Link>
+    <Link to="/blog">blog</Link>
+  </header>
+)
