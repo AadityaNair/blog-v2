2511ae573c6410a43d6d3bd0883b7a10a8fa68ff
AadityaNair <aadityanair6494@gmail.com>
2020-04-17 02:13:50 +0530

    Added basic go back button

--- src/templates/blog-post.jsx
+++ src/templates/blog-post.jsx
@@ -6,6 +6,9 @@ export default ({ data }) => {
   const post = data.markdownRemark
   return (
     <div>
+      <aside>
+        <Link to="/blog">go back</Link>
+      </aside>
       <h1>{post.frontmatter.title}</h1>
       <div dangerouslySetInnerHTML={{ __html: post.html }} />
       <Footer />
