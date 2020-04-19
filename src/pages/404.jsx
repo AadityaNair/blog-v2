91e494cdc5d17d66ed42cec63b8f54ed9a4b58f2
AadityaNair <aadityanair6494@gmail.com>
2020-04-19 13:50:46 +0530

    Basic 404

---
+++ src/pages/404.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import { Link } from "gatsby"
+import Footer from "../components/footer"
+
+export default () => (
+  <div>
+    You seem to have stumbled somewhere weird
+    <Link to="/">Go Home</Link>
+    <Footer />
+  </div>
+)
