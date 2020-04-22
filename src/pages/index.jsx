2b1f17aa118d70a099237e34d60d9be2cb70e3c2
AadityaNair <aadityanair6494@gmail.com>
2020-04-23 00:15:06 +0530

    v1 of a working Front Page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,35 +6,42 @@ import Metadata from "../components/meta"
 import "../styles/mystyle.css"
 
 export default ({ data }) => (
-  <div>
+  <div className="flex flex-col items-center justify-center h-screen ">
     <Metadata
       isBlogPost={false}
       title="The website of Nair"
       description="My experience as a Software Engineer"
     />
+    {/* <div className="flex flex-col justify-center items-center bg-gray-500"> */}
     <Img
-      // className="h-16 w-16 rounded"
+      className="rounded-full"
       fixed={data.file.childImageSharp.fixed}
       alt="author thumb"
     />
-    {/* <h1>{data.site.siteMetadata.title}</h1> */}
-    <h1>
+
+    <h1 className="md:text-5xl text-lg ">
       I'm{" "}
-      <strong className="text-purple-500" itemProp="name">
+      <strong className="" itemProp="name">
         {data.site.siteMetadata.name}
       </strong>
-      , a<em itemProp="jobTitle"> Software Engineer</em>.
+      , a <em itemProp="jobTitle"> Software Engineer</em>.
     </h1>
-    <h3>I love exploring how large systems work</h3>
-    <nav>
-      <Link to="/about">about</Link>
-      <Link to="/blog">blog</Link>
+    <h3 className="md:text-3xl ">I love exploring how large systems work</h3>
+
+    <nav className=" flex flex-row box-border h-20 w-32 items-center justify-between">
+      <Link className="" to="/about">
+        <button>about</button>
+      </Link>
+      <Link className="" to="/blog">
+        <button>blog</button>
+      </Link>
     </nav>
-    <nav>
+    <nav className="w-64 flex flex-row box-border h-20 w-32 items-center justify-between">
       <a href="https://www.linkedin.com/in/aadityanair/">linkedin</a>
       <a href="https://github.com/AadityaNair">github</a>
       <a href="mailto:me@aadityanair.ml">email</a>
     </nav>
+    {/* </div> */}
   </div>
 )
 
