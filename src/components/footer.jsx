35b48d6ca7de6b1f50ab576d56e0d27bf6ae5415
AadityaNair <aadityanair6494@gmail.com>
2020-04-25 00:35:20 +0530

    Alignment doesnt depend on other components

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,8 +2,8 @@ import React from "react"
 import "../styles/mystyle.css"
 
 export default () => (
-  <footer className="">
+  <footer className="items-center justify-center flex mt-auto">
     Copyright {new Date().getFullYear()} by Aaditya M Nair. Built using Gatsby.
-    Source <a href="https://github.com/AadityaNair/blog-v2">here</a>
+    Source <a href="https://github.com/AadityaNair/blog-v2"> here</a>
   </footer>
 )
