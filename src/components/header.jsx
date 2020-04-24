db3b506a26048c3648f4fd9783107f2b191b4ae9
AadityaNair <aadityanair6494@gmail.com>
2020-04-25 02:52:53 +0530

    Bigger text

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,7 +4,7 @@ import "../styles/mystyle.css"
 
 export default props => (
   <header className="items-center justify-center flex flex-col">
-    <h1 className="md:text-4xl text-3xl">
+    <h1 className="md:text-5xl text-4xl">
       <Link to="/">Aaditya M Nair</Link>
     </h1>
     <div className="flex flex-row space-x-6 pt-3">
