1fe7db3dc798a1585d78a36af76b8181c0b5f6ee
AadityaNair <aadityanair6494@gmail.com>
2020-04-25 02:52:38 +0530

    Added CSS for the Blog Index

--- src/templates/blog-index.jsx
+++ src/templates/blog-index.jsx
@@ -6,6 +6,9 @@ import Header from "../components/header"
 import Metadata from "../components/meta"
 import "../styles/mystyle.css"
 
+import leftarrow from "../assets/left.svg"
+import rightarrow from "../assets/right.svg"
+
 function lstags(tagstring) {
   const l = tagstring.split(" ").map(tag => (
     <div>
@@ -20,7 +23,7 @@ export default props => {
   const { pageContext } = props
   const { previousPagePath, nextPagePath } = pageContext
   return (
-    <div className="flex flex-col h-screen bg-blue-100 md:mx-64 mx-8">
+    <div className="flex flex-col h-screen md:mx-64 mx-8">
       <Metadata
         isBlogPost={false}
         title="The Blog of Nair"
@@ -28,26 +31,41 @@ export default props => {
       />
       <Header />
 
-      <h1 className="md:text-5xl text-lg">My own words and stuff</h1>
-      <aside>My opinions are my own and shit</aside>
-      <ol>
+      <h1 className="md:text-4xl text-xl">My own words and stuff</h1>
+      <aside className="mb-8 text-sm">My opinions are my own and shit</aside>
+      <ol className="list-none">
         {data.allMarkdownRemark.edges.map(({ node }, index) => (
-          <li>
+          <li className="p-3">
             <Link to={node.fields.slug}>
-              <h2>{node.frontmatter.title}</h2>
+              <h2 className="text-xl font-black">{node.frontmatter.title}</h2>
             </Link>
-            <details>
-              <time>{node.frontmatter.date}</time>
-              {lstags(node.frontmatter.tags)}
-              <div>{node.timeToRead} min read</div>
-            </details>
+            <div className="flex text-xs md:text-base">
+              <div className="border-solid border-r-1 border-l-2 pr-4 pl-2 ">
+                Published{" "}
+                <time className="italic">{node.frontmatter.date}</time>{" "}
+              </div>
+              <div className="flex flex-row border-solid border-r-1 border-l-2 pl-2 pr-4 space-x-1">
+                {lstags(node.frontmatter.tags)}
+              </div>
+              <div className="border-solid border-l-2 pl-2">
+                {node.timeToRead} min read
+              </div>
+            </div>
             <summary>{node.frontmatter.excerpt}</summary>
           </li>
         ))}
       </ol>
-      <div>
-        {previousPagePath ? <Link to={previousPagePath}>Previous</Link> : null}
-        {nextPagePath ? <Link to={nextPagePath}>Next</Link> : null}
+      <div className="justify-center flex space-x-8">
+        {previousPagePath ? (
+          <Link className="flex flex-row" to={previousPagePath}>
+            <img alt="Previous" src={leftarrow} /> <div>Previous</div>
+          </Link>
+        ) : null}
+        {nextPagePath ? (
+          <Link className="flex flex-row" to={nextPagePath}>
+            <div>Next</div> <img alt="Next" src={rightarrow} />
+          </Link>
+        ) : null}
       </div>
 
       <Footer />
