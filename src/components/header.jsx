359781a3a930030e0a9b1ab6e2161dd5945824ff
AadityaNair <aadityanair6494@gmail.com>
2020-04-24 23:39:23 +0530

    CSS for the headers. Removed the back button

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,19 +1,15 @@
 import React from "react"
 import { Link } from "gatsby"
-import { navigate } from "@reach/router"
 import "../styles/mystyle.css"
 
-const goBack = () => {
-  navigate(-1)
-}
-
 export default props => (
-  <header>
+  <header className="items-center justify-center flex flex-col">
     <h1 className="md:text-4xl text-3xl">
-      <Link to="/">The Blog of Nair</Link>
+      <Link to="/">Aaditya M Nair</Link>
     </h1>
-    <button onClick={goBack}>Go back</button>
-    <Link to="/about">about</Link>
-    <Link to="/blog">blog</Link>
+    <div className="flex flex-row space-x-6 pt-3">
+      <Link to="/about">about</Link>
+      <Link to="/blog">blog</Link>
+    </div>
   </header>
 )
