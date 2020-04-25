1ba48c8975fc98cd89f45d75312032c0838f7d1d
AadityaNair <aadityanair6494@gmail.com>
2020-04-26 03:43:58 +0530

    Removed unused code

--- src/templates/blog-index.jsx
+++ src/templates/blog-index.jsx
@@ -37,26 +37,6 @@ export default props => {
         ))}
       </ol>
 
-      {/* <div className="justify-center flex space-x-8">
-        {previousPagePath ? (
-          <Link className="flex flex-row" to={previousPagePath}>
-            <img alt="Newer" src={leftarrow} /> <div>Newer</div>
-          </Link>
-        ) : (
-          <div className="flex flex-row hidden">
-            <img alt="Newer" src={leftarrow} /> <div>Newer</div>{" "}
-          </div>
-        )}
-        {nextPagePath ? (
-          <Link className="flex flex-row" to={nextPagePath}>
-            <div>Older </div> <img alt="Older" src={rightarrow} />
-          </Link>
-        ) : (
-          <div className="flex flex-row hidden">
-            <div>Older </div> <img alt="Older" src={rightarrow} />{" "}
-          </div>
-        )}
-      </div> */}
       <IndexNavigation prev={previousPagePath} next={nextPagePath} />
 
       <Footer />
