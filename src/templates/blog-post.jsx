ded81d4a298d74ebaf39c273da7fa87e41f93aa0
AadityaNair <aadityanair6494@gmail.com>
2020-04-25 16:17:37 +0530

    Added styling for Title and Metadata from the index page. Setup styling for blog post

--- src/templates/blog-post.jsx
+++ src/templates/blog-post.jsx
@@ -18,7 +18,7 @@ function lstags(tagstring) {
 export default ({ data }) => {
   const post = data.markdownRemark
   return (
-    <div className="flex flex-col items-center justify-between h-screen">
+    <div className="flex flex-col h-screen md:mx-64 mx-8">
       <Metadata
         isBlogPost={true}
         title={post.frontmatter.title}
@@ -27,16 +27,26 @@ export default ({ data }) => {
         description={post.frontmatter.excerpt}
       />
       <Header />
-      <h1>{post.frontmatter.title}</h1>
+
+      <h2 className="text-xl font-black">{post.frontmatter.title}</h2>
       <summary>
-        <p>{lstags(post.frontmatter.tags)}</p>
-        <p>{post.frontmatter.date}</p>
-        <p>{post.timeToRead} min read</p>
-        <div dangerouslySetInnerHTML={{ __html: post.tableOfContents }} />
+        <div className="flex text-xs md:text-base">
+          <div className="border-solid border-r-1 border-l-2 pr-4 pl-2 ">
+            Published <time className="italic">{post.frontmatter.date}</time>{" "}
+          </div>
+          <div className="flex flex-row border-solid border-r-1 border-l-2 pl-2 pr-4 space-x-1">
+            {lstags(post.frontmatter.tags)}
+          </div>
+          <div className="border-solid border-l-2 pl-2">
+            {post.timeToRead} min read
+          </div>
+        </div>
       </summary>
-      <main className="">
+
+      <main id="markdown" className="mt-4 mb-16">
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
       </main>
+      <hr className="mb-16" />
       <Footer />
     </div>
   )
