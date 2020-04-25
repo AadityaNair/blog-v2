a6ed0e1ca1c6c0f86ed445951d9714a8bc7b0745
AadityaNair <aadityanair6494@gmail.com>
2020-04-26 03:43:37 +0530

    Improved logic of tag -> post mapping. All work is done by graphQL now. As it should have been

--- src/pages/tags.jsx
+++ src/pages/tags.jsx
@@ -2,47 +2,20 @@ import React from "react"
 import { graphql } from "gatsby"
 import { Link } from "gatsby"
 
-// The logic flow can probably be improved a *lot*
-// There is probably a graphQL query that solves everything I am doing
-
-// TODO: Also, is this how a JSX page supposed to look. My actual HTML items are
-//       almost invisible within all that JS logic. Ideally, shouldn't all the
-//       "HTML" information be at one place, within the "export default =>" thing?
-
-// Create a set of distinct tags
-function tags(edges) {
-  let s = new Set()
-  for (let index = 0; index < edges.length; index++) {
-    let tagstring = edges[index].node.frontmatter.tags
-    tagstring.split(" ").map(tag => s.add(tag))
-  }
-  let a = Array.from(s)
-  a.sort()
-  return a
-}
-
-function post_for_tag(edges, tag) {
-  let resp = []
-  for (let index = 0; index < edges.length; index++) {
-    let tagset = edges[index].node.frontmatter.tags.split(" ")
-    if (tagset.includes(tag)) {
-      resp.push(edges[index].node)
-    }
-  }
-  return resp.map(post => (
-    <li>
-      <Link to={post.fields.slug}>{post.frontmatter.title}</Link>
-    </li>
-  ))
-}
-
+//TODO: This can probably look better
 export default ({ data }) => (
   <div>
     <ol>
-      {tags(data.allMarkdownRemark.edges).map((tag, index) => (
+      {data.allMarkdownRemark.group.map((tagitem, index) => (
         <li>
-          {tag}
-          <ul>{post_for_tag(data.allMarkdownRemark.edges, tag)}</ul>
+          {tagitem.tag}
+          <ul>
+            {tagitem.nodes.map((post, index2) => (
+              <li>
+                <Link to={post.fields.slug}>{post.frontmatter.title}</Link>
+              </li>
+            ))}
+          </ul>
         </li>
       ))}
     </ol>
@@ -51,12 +24,13 @@ export default ({ data }) => (
 
 export const query = graphql`
   query {
-    allMarkdownRemark(sort: { order: DESC, fields: frontmatter___date }) {
-      edges {
-        node {
+    allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
+      group(field: frontmatter___tags) {
+        tag: fieldValue
+        totalCount
+        nodes {
           frontmatter {
             title
-            tags
           }
           fields {
             slug
