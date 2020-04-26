58bb33b96821d6455a0b1ecef1c0b58ef837e701
AadityaNair <aadityanair6494@gmail.com>
2020-04-26 12:16:36 +0530

    Removed trailing slash in blog posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,7 +7,12 @@ const { createFilePath } = require(`gatsby-source-filesystem`)
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
   if (node.internal.type === `MarkdownRemark`) {
-    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    const slug = createFilePath({
+      node,
+      getNode,
+      basePath: `pages`,
+      trailingSlash: false,
+    })
     createNodeField({
       node,
       name: `slug`,
