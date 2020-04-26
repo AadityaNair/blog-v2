754ccbbd144f485ec84368a2442bf6bdf72adf31
AadityaNair <aadityanair6494@gmail.com>
2020-04-26 12:32:31 +0530

    Fixed properly for now

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,8 +2,10 @@ import React from "react"
 import "../styles/mystyle.css"
 
 export default () => (
-  <footer className="items-center justify-center mt-auto text-xs md:text-sm">
+  <footer className="flex items-center justify-center mt-auto text-xs md:text-sm">
     Copyright {new Date().getFullYear()} by Aaditya M Nair. Source{" "}
-    <a href="https://github.com/AadityaNair/blog-v2">here</a>
+    <a className="ml-1" href="https://github.com/AadityaNair/blog-v2">
+      here
+    </a>
   </footer>
 )
