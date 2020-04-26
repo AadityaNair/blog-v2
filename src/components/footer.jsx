f211a6796a1a4adb897468a166d9bed4deb8b29d
AadityaNair <aadityanair6494@gmail.com>
2020-04-26 12:14:03 +0530

    Fixed footer for mobile. Spacing corrected

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,8 +2,8 @@ import React from "react"
 import "../styles/mystyle.css"
 
 export default () => (
-  <footer className="items-center justify-center flex mt-auto">
-    Copyright {new Date().getFullYear()} by Aaditya M Nair. Built using Gatsby.
-    Source <a href="https://github.com/AadityaNair/blog-v2"> here</a>
+  <footer className="items-center justify-center mt-auto text-xs md:text-sm">
+    Copyright {new Date().getFullYear()} by Aaditya M Nair. Source{" "}
+    <a href="https://github.com/AadityaNair/blog-v2">here</a>
   </footer>
 )
