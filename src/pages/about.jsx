00faa63894929f05e8e345f7130479a4c1d1b0f5
AadityaNair <aadityanair6494@gmail.com>
2020-05-05 02:13:11 +0530

    v1 of about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -13,21 +13,55 @@ export default () => (
     />
     <Header />
     <article className="md:mx-64 mx-8 text-justify font-website">
-      <p>Under Construction</p>
-      But I must explain to you how all this mistaken idea of denouncing
-      pleasure and praising pain was born and I will give you a complete account
-      of the system, and expound the actual teachings of the great explorer of
-      the truth, the master-builder of human happiness. No one rejects,
-      dislikes, or avoids pleasure itself, because it is pleasure, but because
-      those who do not know how to pursue pleasure rationally encounter
-      consequences that are extremely painful. Nor again is there anyone who
-      loves or pursues or desires to obtain pain of itself, because it is pain,
-      but because occasionally circumstances occur in which toil and pain can
-      procure him some great pleasure. To take a trivial example, which of us
-      ever undertakes laborious physical exercise, except to obtain some
-      advantage from it? But who has any right to find fault with a man who
-      chooses to enjoy a pleasure that has no annoying consequences, or one who
-      avoids a pain that produces no resultant pleasure?
+      <section>
+        Hi, I am{" "}
+        <abbr
+          title="No, it just turns red. No link here"
+          className="text-highlight"
+        >
+          Aaditya M Nair
+        </abbr>
+        . I currently live in Gurugram, India.
+      </section>
+      <section>
+        I am currently working with{" "}
+        <a href="https://tower-research.com"> Tower Research Capital </a>
+        as a Software Engineer. Here, I work with the recently formed
+        Information Security team. Me and my team are responsible for making and
+        enforcing all policies related to security and compliance. We also
+        collaborate with other teams improve observability into the tools they
+        manage, identify and remediate any security bugs we find while also
+        promoting security by design on new projects. This is my first actual
+        job post college :-).
+      </section>
+      {/* TODO: Links to relevant pages */}
+      <section>
+        In College though, I was pursuing a Bachelors + Masters dual degree
+        program in Computer Science at{" "}
+        <a href="https://iiit.ac.in">IIIT Hyderabad</a>. For my Master's Thesis
+        (for which I am required to publish a few papers), I worked with{" "}
+        <a href="https://faculty.iiit.ac.in/~lalitha.v/">
+          Dr Lalitha Vadlamani
+        </a>{" "}
+        on Erasure Codes. More specifically, I worked on Codes with Hierarchical
+        Locality. While in College, I was also part of the sysadmin team which
+        managed various services (including EMail, proxy, Identity) for about
+        3000 students and professors at any given time. It was probably this job
+        that sparked my interest in all things systems and networks.
+      </section>
+      <section>
+        I started this website to have kind of an online presence (and because
+        all the cool kids are doing it). This website is supposed to be the
+        landing site for anyone who wants to find out more about me. That is
+        pretty much why I wrote this page. I also started a blog where I will
+        (try to) document everything I am currently studying. There is a lot to
+        be said about jotting everything down. It requires you to have a clear
+        mental picture yourself which is very important with CS concepts in
+        general. It also acts as documentation for when you want to refer to it
+        in future. With that said, the posts can just be small snippets of
+        interesting code I found or it can also be an essay on some topic I
+        found interesting.
+      </section>
     </article>
     <Footer />
   </div>
