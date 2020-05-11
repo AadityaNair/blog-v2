f103b71588a8107e388e2df2d1c6d7504766c3f1
AadityaNair <aadityanair6494@gmail.com>
2020-05-11 13:37:46 +0530

    Improved text sizes

--- src/pages/tags.jsx
+++ src/pages/tags.jsx
@@ -10,15 +10,15 @@ export default ({ data }) => (
   <div className="flex flex-col h-screen md:mx-64 mx-8 mt-16 font-website text-text">
     <Metadata isBlogPost={false} title="Tag Index" description={null} />
 
-    <h1 className="text-3xl mb-4">All Posts by Tags</h1>
+    <h1 className="text-3xl md:text-5xl mb-4">All Posts by Tags</h1>
 
     <ol className="flex flex-col">
       {data.allMarkdownRemark.group.map((tagitem, index) => (
-        <li className="font-bold uppercase mb-4">
+        <li className="font-bold uppercase mb-4 md: text-3xl">
           {tagitem.tag}
           <ul className="ml-4 border-solid border-lines border-l-2 pl-2">
             {tagitem.nodes.map((post, index2) => (
-              <li className="normal-case font-normal">
+              <li className="normal-case font-normal md:text-xl">
                 <Link to={post.fields.slug}>{post.frontmatter.title}</Link>
               </li>
             ))}
