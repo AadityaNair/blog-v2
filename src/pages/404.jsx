4b1affb8afc3adfd8ccd81b83ee59597d2264d5f
AadityaNair <aadityanair6494@gmail.com>
2020-05-11 18:50:20 +0530

    Fix font size

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -7,7 +7,7 @@ export default () => (
   <div className="flex flex-col items-center justify-between h-screen text-text">
     <Metadata isBlogPost={false} title="Lost..." description={null} />
 
-    <main className="flex flex-col items-center m-auto md:text-3xl text-xl font-extrabold font-website">
+    <main className="flex flex-col items-center m-auto md:text-5xl text-xl font-extrabold font-website">
       You have reached the world's edge. None but devils play past here....
       <Link to="/" className="text-highlight">
         Turn Back
