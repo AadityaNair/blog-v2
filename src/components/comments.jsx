73391d1cb55d9ecbd7976e717535ef3600ab40f5
AadityaNair <aadityanair6494@gmail.com>
2020-05-14 23:26:39 +0530

    Add a button to load comments

--- src/components/comments.jsx
+++ src/components/comments.jsx
@@ -1,20 +1,30 @@
-import React, { Component } from "react"
+import React from "react"
 
-export default class Comments extends Component {
-  componentDidMount() {
-    let script = document.createElement("script")
-    let anchor = document.getElementById("comment")
-    script.setAttribute("src", "https://utteranc.es/client.js")
-    script.setAttribute("crossorigin", "anonymous")
-    script.setAttribute("async", true)
-    script.setAttribute("repo", "AadityaNair/blog-v2")
-    script.setAttribute("issue-term", "title")
-    script.setAttribute("theme", "dark-blue")
-    script.setAttribute("label", "utterance")
-    anchor.appendChild(script)
-  }
+function loadComment(props) {
+  let script = document.createElement("script")
+  let anchor = document.getElementById("comment")
+  script.setAttribute("src", "https://utteranc.es/client.js")
+  script.setAttribute("crossorigin", "anonymous")
+  script.setAttribute("async", true)
+  script.setAttribute("repo", "AadityaNair/blog-v2")
+  script.setAttribute("issue-term", "title")
+  script.setAttribute("theme", "dark-blue")
+  script.setAttribute("label", "utterance")
+  anchor.appendChild(script)
 
-  render() {
-    return <div id="comment"></div>
-  }
+  // TODO: Make this button a show/hide thing
+  let button = document.getElementById("comment-button")
+  button.remove()
 }
+
+function Comment(props) {
+  return (
+    <div id="comment">
+      <button id="comment-button" onClick={loadComment}>
+        Load Comments
+      </button>
+    </div>
+  )
+}
+
+export default Comment
