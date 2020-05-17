a896c68e63ebbfaa1cf64775ea9a8782bb0b9b03
AadityaNair <aadityanair6494@gmail.com>
2020-05-17 13:14:53 +0530

    Change code block theme with the website

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,10 @@
-require("./src/styles/prism-dracula.css")
+const config = require("./conf")
+
 require("prismjs/plugins/line-numbers/prism-line-numbers.css")
 require("prismjs/plugins/command-line/prism-command-line.css")
+
+if (config.websiteTheme === "light") {
+  require("prismjs/themes/prism-solarizedlight.css")
+} else {
+  require("./src/styles/prism-dracula.css")
+}
