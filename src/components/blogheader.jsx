5ff9a2eb2c32fed41af4dcda6edfce6c1100fa9d
AadityaNair <aadityanair6494@gmail.com>
2020-05-31 01:37:02 +0530

    Added space between multiple tags. use divide-width

--- src/components/blogheader.jsx
+++ src/components/blogheader.jsx
@@ -16,17 +16,14 @@ export default (props) => (
       <h1 className="text-3xl font-black font-head text-link">{props.title}</h1>
     </Link>
     <summary>
-      {/* TODO: https://tailwindcss.com/docs/divide-width/ */}
-      <div className="flex text-xs md:text-base font-subtext">
-        <div className="border-solid border-r-1 border-l-2 pr-4 pl-2 border-lines">
+      <div className="flex text-xs md:text-base font-subtext divide-x-2">
+        <div className="border-solid border-r-1 border-l-2 pl-2 pr-4 border-lines">
           Published <time dateTime={props.compDate}>{props.date}</time>{" "}
         </div>
-        <div className="flex flex-row border-solid border-r-1 border-l-2 pl-2 pr-4 space-x-1 border-lines italic underline">
+        <div className="flex flex-row space-x-2 pl-2 pr-4 border-lines italic underline">
           {lstags(props.tags)}
         </div>
-        <div className="border-solid border-l-2 pl-2 border-lines">
-          {props.timeToRead} min read
-        </div>
+        <div className="border-lines pl-2">{props.timeToRead} min read</div>
       </div>
     </summary>
   </Fragment>
