2f59007dfc9757f37f133765d81653cd7147475e
AadityaNair <aadityanair6494@gmail.com>
2020-12-04 18:25:10 +0530

    Replaced the deprecated gatsby-prefetch-google-fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,16 +75,18 @@ dev_plugins = [
   {
     // TODO: Performance: Reduce fonts download to the exact style(or even character) we need
     // TODO Try using the NPM packages or webfont-loader whichever works
-    resolve: `gatsby-plugin-prefetch-google-fonts`,
+    resolve: `gatsby-plugin-web-font-loader`,
     options: {
-      fonts: [
-        { family: `Alegreya Sans` },
-        { family: `Cantarell` },
-        { family: `IBM Plex Mono` },
-        { family: `Merriweather Sans` },
-        { family: `Source Code Pro` },
-        { family: `Source Sans Pro`, variants: ["400", "400i"] },
-      ],
+      google: {
+        families: [
+          "Alegreya Sans",
+          "Cantarell",
+          "IBM Plex Mono",
+          "Merriweather Sans",
+          "Source Code Pro",
+          "Source Sans Pro:400,400:italic",
+        ],
+      },
     },
   },
 ]
@@ -165,33 +167,33 @@ prod_plugins = [
     },
   },
   {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: "UA-169274463-1",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        // exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Enables Google Optimize using your container Id
-        //optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-        // Enables Google Optimize Experiment ID
-        //experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-        // Set Variation ID. 0 for original 1,2,3....
-        //variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-        // Defers execution of google analytics script after page load
-        defer: true,
-        // Any additional optional fields
-        //sampleRate: 5,
-        //siteSpeedSampleRate: 10,
-        //cookieDomain: "example.com",
-      },
+    resolve: `gatsby-plugin-google-analytics`,
+    options: {
+      trackingId: "UA-169274463-1",
+      // Defines where to place the tracking script - `true` in the head and `false` in the body
+      head: false,
+      // Setting this parameter is optional
+      anonymize: true,
+      // Setting this parameter is also optional
+      respectDNT: true,
+      // Avoids sending pageview hits from custom paths
+      // exclude: ["/preview/**", "/do-not-track/me/too/"],
+      // Delays sending pageview hits on route update (in milliseconds)
+      pageTransitionDelay: 0,
+      // Enables Google Optimize using your container Id
+      //optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+      // Enables Google Optimize Experiment ID
+      //experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+      // Set Variation ID. 0 for original 1,2,3....
+      //variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+      // Defers execution of google analytics script after page load
+      defer: true,
+      // Any additional optional fields
+      //sampleRate: 5,
+      //siteSpeedSampleRate: 10,
+      //cookieDomain: "example.com",
     },
+  },
   `gatsby-plugin-netlify-cache`,
   `gatsby-plugin-preact`,
 ]
